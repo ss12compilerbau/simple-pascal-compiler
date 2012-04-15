307d2881aa5326885e3ed92df95ecd1db3a22299
Reinhold Kolm <reinhold.kolm@gmx.at>
2012-04-15 09:35:04 +0200

    procedure call

--- ParseWrapper.pas
+++ ParseWrapper.pas
@@ -1,6 +1,6 @@
 PROGRAM SPC;
 
-	Var debugmode: boolean;
+	// Var debugmode: boolean;
 
 {$include 'scanner.pas';}
 {$include 'symboltable.pas';}
@@ -8,24 +8,27 @@ PROGRAM SPC;
  
 
 	BEGIN
-		debugmode := true;
+		//debugmode := true;
+		parserUseSymTab := cFalse;
+		parserPrintSymTab := cTrue; 
 
-	    scannerInit();
-	    parserInit();
-    	STInit();
+	    (scannerInit());
+	    (parserInit());
+    	(STInit());
 
 
     if ParamCount < 1 then
     begin
-        writeln('Not enough parameters given. Usage: ' + ParamStr(0) + ' input.pas output.out');
-        halt(1);
+        (writeln('Not enough parameters given. Usage: ' + ParamStr(0) +
+			' input.pas output.out'));
+        (halt(1));
     end
     else begin
 		(* scan( ParamStr(1), ParamStr(2) ) *)
-		printSymbolTable(stSymbolTable, '');
-		stInsertSymbol('I', stVar, cFalse, 'LONGINT');
-		parse( ParamStr(1));
-		printSymbolTable(stSymbolTable, '');
+		(printSymbolTable(stSymbolTable, ''));
+		//stInsertSymbol('I', stVar, cFalse, 'LONGINT');
+		(parse( ParamStr(1)));
+		//printSymbolTable(stSymbolTable, '');
     end;
 
   END.
